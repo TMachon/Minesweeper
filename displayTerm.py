b76d028003e9860c974b595265ae04684de1780f
TMachon <theo.machon.91@gmail.com>
2020-01-23 17:46:34 +0100

    Changed 2 2D tables into a dictionary pt2

--- displayTerm.py
+++ displayTerm.py
@@ -1,52 +1,5 @@
 # coding: utf-8
 
-class TerminalDisplay:
-    
-    def output(self, tableForBombs, tableForClicks):
-
-        height = len(tableForBombs)
-        width = len(tableForBombs[0])
-
-        splitLine = "  "
-        for i in range(width):
-            splitLine += "┼───"
-        splitLine += "┤"
-
-        endLine = "  "
-        for i in range(width):
-            endLine += "┴───"
-        endLine += "┘"
-
-        line = "   "
-        for i in range(width):
-            head = " "+str(i+1) if len(str(i+1))==1 else str(i+1)
-            line += head+"  "
-        
-        print(line)
-
-        for j in range(height):
-
-            print(splitLine)
-
-            head = " "+str(j+1) if len(str(j+1))==1 else str(j+1)
-            line = head+"│"
-            for i in range(width):
-                #if (tableForClicks[j][i]):
-                if (True):
-                    val = "•" if tableForBombs[j][i]>=9 else str(tableForBombs[j][i])
-                else:
-                    val = "□"
-                #val=str(tableForClicks[j][i])[0]
-                line += " "+val+" │"
-                print
-
-            print(line)
-        
-        print(endLine)
-
-    def input(self, message):
-        return input(message)
-
 class TerminalDisplayV2:
     
     def gameDisplay(self, dictionary):
@@ -99,6 +52,64 @@ class TerminalDisplayV2:
             for i in range(width):
                 print(str("{0:0=2d}".format(dictionary[j, i, 0]))+":"+str(dictionary[j, i, 1])[0]+" ", end='')
             print("")
+
+    def input(self, message):
+        return input(message)
+
+
+
+
+
+
+
+
+#
+# Legacy
+#
+
+class TerminalDisplay:
+    
+    def output(self, tableForBombs, tableForClicks):
+
+        height = len(tableForBombs)
+        width = len(tableForBombs[0])
+
+        splitLine = "  "
+        for i in range(width):
+            splitLine += "┼───"
+        splitLine += "┤"
+
+        endLine = "  "
+        for i in range(width):
+            endLine += "┴───"
+        endLine += "┘"
+
+        line = "   "
+        for i in range(width):
+            head = " "+str(i+1) if len(str(i+1))==1 else str(i+1)
+            line += head+"  "
+        
+        print(line)
+
+        for j in range(height):
+
+            print(splitLine)
+
+            head = " "+str(j+1) if len(str(j+1))==1 else str(j+1)
+            line = head+"│"
+            for i in range(width):
+                #if (tableForClicks[j][i]):
+                if (True):
+                    val = "•" if tableForBombs[j][i]>=9 else str(tableForBombs[j][i])
+                else:
+                    val = "□"
+                #val=str(tableForClicks[j][i])[0]
+                line += " "+val+" │"
+                print
+
+            print(line)
+        
+        print(endLine)
 
     def input(self, message):
         return input(message)
--- minesweeper.py
+++ minesweeper.py
@@ -7,102 +7,73 @@ class Minesweeper:
     def __init__(self):
 
         ## Init atributes
-        self.display = displayTerm.TerminalDisplay()
         self.display2 = displayTerm.TerminalDisplayV2()
         self.diff = 0   # 0: null, 1: easy, 2: medium, 3:hard
         self.width = 0
         self.height = 0
         self.nbBombs = 0
         self.nbDiscovered = 0
-        self.bombTable = [] # int 2D table with bomb postions (maked as 9) and case value (0-8)
-        self.viewTable = [] # bool 2D table with discovered cases (True=discovered, False=hidden)
-        self.field = {} # as in a minefield
+
+        self.field = {} # "field" as in minefield
+        # (y, x, O): (int) bomb positions (maked as 9 or above) and case value (0-8)
+        # (y, x, 1): (bool) discovered cases (True=discovered, False=hidden)
 
     def _settings(self):
 
         ## Set difficulty level
         while not (1<=self.diff<=3) :
-            self.diff = int(self.display.input("Select difficulty (1, 2, 3): "))
+            self.diff = int(self.display2.input("Select difficulty (1, 2, 3): "))
 
         ## Set dimensions
         while not (5<=self.height<=99) :
-            self.height = int(self.display.input("Select height (between 5 and 99): "))
+            self.height = int(self.display2.input("Select height (between 5 and 99): "))
 
         while not (5<=self.width<=99) :
-            self.width = int(self.display.input("Select width (between 5 and 99): "))
+            self.width = int(self.display2.input("Select width (between 5 and 99): "))
 
         ## Generate bombs
         if (self.diff == 1): self.nbBombs = int(self.width*self.height/5)
         elif (self.diff == 2): self.nbBombs = int(self.width*self.height/3.2)
         elif (self.diff == 3): self.nbBombs = int(self.width*self.height/2.5)
+
         print(self.nbBombs) #DEBUG
    
         bombsPos = random.sample(range(self.width*self.height), self.nbBombs)
+
         print(bombsPos) #DEBUG
 
         ## Initialize tables and bombs
 
         for i in range(self.height):
-            bombLine = []
-            viewLine = []
             for j in range(self.width):
                 if ((i*self.width)+j in bombsPos):
                     self.field[i, j, 0] = 9
-                    bombLine.append(9)
                 else:
-                    bombLine.append(0)
                     self.field[i, j, 0] = 0
-                viewLine.append(False)
                 self.field[j, i, 1] = False
-            self.bombTable.append(bombLine)
-            self.viewTable.append(viewLine)
-
-        #
-        self.display.output(self.bombTable, self.viewTable)
-        #
 
         ## Place numbers
 
         # top left corner
-        if (self.bombTable[0][0] >=9):
-
-            self.bombTable[0][1] += 1
-            self.bombTable[1][0] += 1
-            self.bombTable[1][1] += 1
-
+        if (self.field[0, 0, 0] >=9):
             self.field[0, 1, 0] += 1
             self.field[1, 0, 0] += 1
             self.field[1, 1, 0] += 1
 
         # top right corner
-        if (self.bombTable[0][self.width-1] >= 9):
-
-            self.bombTable[0][self.width-2] += 1
-            self.bombTable[1][self.width-2] += 1
-            self.bombTable[1][self.width-1] += 1
-
+        if (self.field[0, self.width-1, 0] >= 9):
             self.field[0, self.width-2, 0] += 1
             self.field[1, self.width-2, 0] += 1
             self.field[1, self.width-1, 0] += 1
 
         # bottom left corner
-        if (self.bombTable[self.height-1][0] >= 9):
-
-            self.bombTable[self.height-2][0] += 1
-            self.bombTable[self.height-2][1] += 1
-            self.bombTable[self.height-1][1] += 1
-
+        if (self.field[self.height-1, 0, 0] >= 9):
             self.field[self.height-2, 0, 0] += 1
             self.field[self.height-2, 1, 0] += 1
             self.field[self.height-1, 1, 0] += 1
 
         # bottom right corner
-        if (self.bombTable[self.height-1][self.width-1] >= 9):
-
-            self.bombTable[self.height-2][self.width-2] += 1
-            self.bombTable[self.height-2][self.width-1] += 1
-            self.bombTable[self.height-1][self.width-2] += 1
-
+        if (self.field[self.height-1, self.width-1, 0] >= 9):
             self.field[self.height-2, self.width-2, 0] += 1
             self.field[self.height-2, self.width-1, 0] += 1
             self.field[self.height-1, self.width-2, 0] += 1
@@ -110,28 +81,14 @@ class Minesweeper:
 
         # top & bottom edges
         for i in range(1, self.width-1):
-            if (self.bombTable[0][i] >= 9):
-
-                self.bombTable[0][i-1] += 1
-                self.bombTable[0][i+1] += 1
-                self.bombTable[1][i-1] += 1
-                self.bombTable[1][i] += 1
-                self.bombTable[1][i+1] += 1
-
+            if (self.field[0, i, 0] >= 9):
                 self.field[0, i-1, 0] += 1
                 self.field[0, i+1, 0] += 1
                 self.field[1, i-1, 0] += 1
                 self.field[1, i, 0] += 1
                 self.field[1, i+1, 0] += 1
 
-            if (self.bombTable[self.height-1][i] >= 9):
-
-                self.bombTable[self.height-2][i-1] += 1
-                self.bombTable[self.height-2][i] += 1
-                self.bombTable[self.height-2][i+1] += 1
-                self.bombTable[self.height-1][i-1] += 1
-                self.bombTable[self.height-1][i+1] += 1
-
+            if (self.field[self.height-1, i, 0] >= 9):
                 self.field[self.height-2, i-1, 0] += 1
                 self.field[self.height-2, i, 0] += 1
                 self.field[self.height-2, i+1, 0] += 1
@@ -140,28 +97,14 @@ class Minesweeper:
         
         # left & right edges
         for i in range(1, self.height-1):
-            if (self.bombTable[i][0] >= 9):
-
-                self.bombTable[i-1][0] += 1
-                self.bombTable[i-1][1] += 1
-                self.bombTable[i][1] += 1
-                self.bombTable[i+1][0] += 1
-                self.bombTable[i+1][1] += 1
-
+            if (self.field[i, 0, 0] >= 9):
                 self.field[i-1, 0, 0] += 1
                 self.field[i-1, 1, 0] += 1
                 self.field[i, 1, 0] += 1
                 self.field[i+1, 0, 0] += 1
                 self.field[i+1, 1, 0] += 1
 
-            if (self.bombTable[i][self.width-1] >= 9):
-
-                self.bombTable[i-1][self.width-2] += 1
-                self.bombTable[i-1][self.width-1] += 1
-                self.bombTable[i][self.width-2] += 1
-                self.bombTable[i+1][self.width-2] += 1
-                self.bombTable[i+1][self.width-1] += 1
-
+            if (self.field[i, self.width-1, 0] >= 9):
                 self.field[i-1, self.width-2, 0] += 1
                 self.field[i-1, self.width-1, 0] += 1
                 self.field[i, self.width-2, 0] += 1
@@ -171,17 +114,7 @@ class Minesweeper:
         # middle
         for i in range(1, self.height-1):
             for j in range(1, self.width-1):
-                if (self.bombTable[i][j] >= 9):
-
-                    self.bombTable[i-1][j-1] += 1
-                    self.bombTable[i-1][j] += 1
-                    self.bombTable[i-1][j+1] += 1
-                    self.bombTable[i][j-1] += 1
-                    self.bombTable[i][j+1] += 1
-                    self.bombTable[i+1][j-1] += 1
-                    self.bombTable[i+1][j] += 1
-                    self.bombTable[i+1][j+1] += 1
-                    
+                if (self.field[i, j, 0] >= 9):
                     self.field[i-1, j-1, 0] += 1
                     self.field[i-1, j, 0] += 1
                     self.field[i-1, j+1, 0] += 1
@@ -201,20 +134,17 @@ class Minesweeper:
         if (y >= self.height or y < 0):
             print("NONO")
 
-        if (self.bombTable[y-1][x] == 9):
+        if (self.field[y-1, x, 0] == 9):
             self.gameLost()
         else:
             self._discoverRecursive(x, y)
 
     def _discoverRecursive(self, x, y):
-        # Reminder:
-        #  self.bombTable = [] #int 2D table with bomb postions (maked as 9) and case value (0-8)
-        #  self.viewTable = [] #bool 2D table with discovered cases
         x -= 1
         y -= 1
         if (x >= 0 and x < self.width and y >= 0 and y < self.width):
-            self.viewTable[y][x] == True
-            if (self.bombTable[y][x] == 0):
+            self.field[y, x, 1] == True
+            if (self.field[y, x, 0] == 0):
                 self._discoverRecursive(x-1, y-1)
                 self._discoverRecursive(x-1, y)
                 self._discoverRecursive(x-1, y+1)
@@ -233,14 +163,14 @@ class Minesweeper:
     def start(self):
 
         self._settings()
-        self.display2.gameDisplay(self.field)
-        self.display2.debugDisplay(self.field)
+
+        self.display2.debugDisplay(self.field) #Debug
                 
         ## Main game loop
         while True :
-            self.display.output(self.bombTable, self.viewTable)
+            self.display2.gameDisplay(self.field)
             val = ""
             while not (re.match("^[0-9]{1,2}:[0-9]{1,2}$", val)) :
-                val = self.display.input("Type in the case (X:Y) that you want to discover: ")
+                val = self.display2.input("Type in the case (X:Y) that you want to discover: ")
 
 Minesweeper().start()
